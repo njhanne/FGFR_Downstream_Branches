350cbf0b6dbf1300fc4900565e915a9be28981b0
nicholas <njhanne@gmail.com>
2025-02-03 12:40:42 -0800

    Updated proliferation code to include DMSO control samples

--- Proliferation/R/proliferation.R
+++ Proliferation/R/proliferation.R
@@ -18,7 +18,7 @@ df_cellpose <- df_cellpose %>% mutate(proliferation = phh3_count / nuclei_count 
 df_cellpose$side <- factor(df_cellpose$side, levels = c("treated", "control"))
 df_cellpose$side <- factor(df_cellpose$side, labels = c("treated", "contralateral"))
 df_cellpose <- rename(df_cellpose, treatment = group)
-df_cellpose$treatment <- factor(df_cellpose$treatment, levels = c("U0126", "LY294002", "U73122"))
+df_cellpose$treatment <- factor(df_cellpose$treatment, levels = c("DMSO", "U0126", "LY294002", "U73122"))
 
 ### Compare treated-control side
 # Should use Student's paired t-test since we want difference within each sample
@@ -29,7 +29,8 @@ df_cellpose <- df_cellpose %>% group_by(treatment, side, sample) %>% summarise_a
 df_cellpose <- df_cellpose %>% filter(sample != 'U73_21')
 
 # run the t-tests - this gives pvalues used in manuscript
-by(df_cellpose, df_cellpose$treatment, function(x) t.test(x$proliferation ~ x$side, paired=TRUE, data=x))
+df_cellpose_no_DMSO <- df_cellpose %>% filter(treatment != 'DMSO')
+by(df_cellpose_no_DMSO, df_cellpose_no_DMSO$treatment, function(x) t.test(x$proliferation ~ x$side, paired=TRUE, data=x))
 
 cellpose_summarise <- summarise(group_by(df_cellpose, treatment,side), mean=mean(proliferation),sd=sd(proliferation))
 cellpose_summarise <- cellpose_summarise %>% unite('treatment_side', c('treatment', 'side'), remove=FALSE)
@@ -38,12 +39,26 @@ pdf("./figs/cellpose_pHH3.pdf", width = 7.5, height = 6)
 p <- ggplot() + geom_bar(data = cellpose_summarise, aes(y=mean, x = side), stat="identity") +
   geom_jitter(data = df_cellpose, aes(x = side, y = proliferation), shape=16) +
   geom_errorbar(data = cellpose_summarise, aes(y=mean,x=side,ymin=mean-sd,ymax=mean+sd)) +
-  facet_wrap(~ treatment)
+  facet_wrap(~ treatment, nrow=1)
 # file_name <- paste("cellpose_pHH3.png")
 # ggsave(filename=file_name, p, width = 15, heigh = 25, units='cm')
 print(p)
 dev.off()
 
+
+# ANOVA
+### main effects only
+test <- aov(proliferation ~ treatment, data= df_cellpose)
+summary(test)
+TukeyHSD(test)
+
+### side
+test <- aov(proliferation ~ treatment*side, data= df_cellpose)
+summary(test)
+TukeyHSD(test)
+
+
+### Now that we have DMSO I don't think we need these comparisons anymore
 ### Looks like the U0126 don't have significant difference but the control side seems 'affected'
 # Will use Welch's t test as the sample sizes are different
 df_cellpose_temp <- df_cellpose %>% mutate(welch_sort = case_when(treatment == 'U0126' ~ 'U0126',
--- Proliferation/python/rgb_to_stack.py
+++ Proliferation/python/rgb_to_stack.py
@@ -43,34 +43,40 @@ image_info['sample_side'] = image_info[cols].astype(str).apply('_'.join, axis=1)
 
 # loop through images, create the stacks, and save them
 for image_pair_name in image_info['sample_side'].unique():
+  skip = False
   # match sample to the manual info from your .csv
   manual_info = image_info.loc[image_info['sample_side'] == image_pair_name] # find all the rgb images that match the sample&side combo
   stack_size = manual_info.shape[0] # counts number of rgb images that will makeup stack. Should always be 2 for this project
 
-  # create the stack
-  for idx, info in enumerate(manual_info.itertuples(),0):
-    image_name = info.old_filename
-    image_path = [image_path for image_path in image_paths if Path(image_path).name == image_name]
-    image = cv2.imread(str(image_path[0])) # read rgb image
-    if len(image.shape) == 3 & image.shape[2] == 3:
-      image = image[..., ::-1]  # convert from BGR to RGB... this is a weird opencv convention
+  # optional to only get certain group, comment this out if you want all the images to run
+  # if manual_info['group'].iloc[0] != 'DMSO':
+  #   skip = True
 
-    if idx == 0:
-      stack = np.empty([image.shape[0], image.shape[1], stack_size]) # create the stack (empty array)
-    if bool(re.search('\d{1,}a.tif', image_name)): # look for the image ending in 'a', this is the pHH3 image
-      stack[:,:,1] = manipulate_image(image, info) # add it as second image in stack
+  if not skip:
+    # create the stack
+    for idx, info in enumerate(manual_info.itertuples(),0):
+      image_name = info.old_filename
+      image_path = [image_path for image_path in image_paths if Path(image_path).name == image_name]
+      image = cv2.imread(str(image_path[0])) # read rgb image
+      if len(image.shape) == 3 & image.shape[2] == 3:
+        image = image[..., ::-1]  # convert from BGR to RGB... this is a weird opencv convention
+
+      if idx == 0:
+        stack = np.empty([image.shape[0], image.shape[1], stack_size]) # create the stack (empty array)
+      if bool(re.search('\d{1,}a.tif', image_name)): # look for the image ending in 'a', this is the pHH3 image
+        stack[:,:,1] = manipulate_image(image, info) # add it as second image in stack
+      else:
+        stack[:,:,0] = manipulate_image(image, info) # add nuclei image as first image in stack
+
+    # find image magnification from old filename
+    magnification = re.search('\d{1,}x', manual_info['old_filename'].iloc[0]) # gets magnification
+    if magnification is None:
+      magnification = ''
     else:
-      stack[:,:,0] = manipulate_image(image, info) # add nuclei image as first image in stack
+      magnification = magnification[0]
 
-  # find image magnification from old filename
-  magnification = re.search('\d{1,}x', manual_info['old_filename'].iloc[0]) # gets magnification
-  if magnification is None:
-    magnification = ''
-  else:
-    magnification = magnification[0]
-
-  new_filename = manual_info['sample_side'].iloc[0] + '_' + magnification
-  savename = (output_directory / (new_filename + '.tif')).resolve()
-  # OME-TIFF should be TZCYX (frustrating) so we need to transpose
-  stack = np.moveaxis(stack, -1, 0) # moves the last (-1) dimension to the first dimension (0) so instead of yxz it will be cyx
-  imwrite(savename, stack.astype('uint8'))
+    new_filename = manual_info['sample_side'].iloc[0] + '_' + magnification
+    savename = (output_directory / (new_filename + '.tif')).resolve()
+    # OME-TIFF should be TZCYX (frustrating) so we need to transpose
+    stack = np.moveaxis(stack, -1, 0) # moves the last (-1) dimension to the first dimension (0) so instead of yxz it will be cyx
+    imwrite(savename, stack.astype('uint8'))
