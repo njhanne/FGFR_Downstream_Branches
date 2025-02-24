19e1a6f601ac3a13b40d99612fc18e66384a7e88
njhanne <njhanne@gmail.com>
2025-02-24 15:12:23 -0800

    Dab quant counting code progress - nearly done

--- Pathway_activation/python/pathway_activation_counting.py
+++ Pathway_activation/python/pathway_activation_counting.py
@@ -1,7 +1,8 @@
-import cv2
 import numpy as np
 import pandas as pd
 from PIL import Image, ImageDraw
+import skimage.measure
+import skimage.io
 
 from read_roi import  read_roi_file # pip install read-roi
 
@@ -75,23 +76,24 @@ def composite_roi_mask(mask_img, roi):
 
 ### MAIN ###
 # First find all needed directories and load them all up
-data_dir = (Path.cwd().parent.parent / 'data' / 'Apoptosis').resolve()
-nuclei_directory = (data_dir / 'cellpose' / 'nuclei').resolve()
-tunel_directory = (data_dir / 'cellpose' / 'TUNEL').resolve()
-roi_directory = (data_dir / 'cellpose' / 'rois').resolve()
+data_dir = (Path.cwd().parent.parent / 'data' / 'Branches_activation').resolve()
+dab_directory = (data_dir / 'analysis' / 'deconvolved').resolve()
+dab_mask_directory = (data_dir / 'analysis' / 'cellpose' / 'DAB').resolve()
+
 
 ## Get sample info
 # I use a csv file with info on all the images to match them to the samples and if they are nuclei or tunel images
-sample_info = pd.read_csv((data_dir / 'TUNEL_image_log.csv').resolve())
-cols = ['sample', 'side', 'section', 'mag']
-sample_info['sample_side'] = sample_info[cols].astype(str).apply('_'.join, axis=1)
+sample_info = pd.read_csv((data_dir / 'samples.csv').resolve())
 
-## find all the cellpose mask files created in 'run_cellpose_TUNEL.py'
-nuclei_dirs, nuclei_mask_paths = find_all_filepaths(nuclei_directory, '.png')
-nuclei_mask_paths = [mp for mp in nuclei_mask_paths if Path(mp).name.endswith('_masks.png')]
-tunel_dirs, tunel_mask_paths = find_all_filepaths(tunel_directory, '.png')
-tunel_mask_paths = [mp for mp in tunel_mask_paths if Path(mp).name.endswith('_masks.png')]
-roi_dirs, roi_paths = find_all_filepaths(roi_directory, '.roi')
+# thresh is just the mean of the pos and neg selected cells
+sample_info['mean_thresh'] = sample_info[['pos1','pos2', 'pos3', 'neg1', 'neg2', 'neg3']].mean(axis=1)
+
+## find all the orignal images, cellpose mask files, and rois
+sample_dirs, sample_img_paths = find_all_filepaths(dab_directory, '.tiff')
+sample_img_paths = [sp for sp in sample_img_paths if Path(sp).name.endswith('_deconvolved.tif')] # we don't want the thresh ones
+dab_dirs, dab_mask_paths = find_all_filepaths(dab_mask_directory, '.png')
+dab_mask_paths = [mp for mp in dab_mask_paths if Path(mp).name.endswith('_masks.png')]
+roi_dirs, roi_paths = find_all_filepaths(dab_directory, '.roi')
 
 ## Unfortunately this analysis will be a bit more complicated than the proliferation analysis.
 ## There is a lot of apoptosis in the neurectoderm and ectoderm that we don't want to quantify as being an effect of the
@@ -100,44 +102,50 @@ roi_dirs, roi_paths = find_all_filepaths(roi_directory, '.roi')
 ## we're going to build the results in a loop, then convert to a dataframe for saving
 results = []
 
-for sample in sample_info['sample_side'].unique():
+for sample in sample_info['new_filename'].unique():
   # match the sample name to the correct images and roi file
-  nuc_img_path = [mp for mp in nuclei_mask_paths if Path(mp).name.startswith(sample)]
-  stain_img_path = [mp for mp in tunel_mask_paths if Path(mp).name.startswith(sample)]
+  dab_img_path = [mp for mp in dab_mask_paths if Path(mp).name.startswith(sample)]
+  sample_img_path = [sp for sp in sample_img_paths if Path(sp).name.startswith(sample)]
   roi_path = [rp for rp in roi_paths if Path(rp).name.startswith(sample)]
 
   if len(roi_path) != 0:
     # load the images and roi file
-    nuc_img = cv2.imread(str(nuc_img_path[0]), -1) # https://docs.opencv.org/3.4/d8/d6a/group__imgcodecs__flags.html
-    stain_img = cv2.imread(str(stain_img_path[0]), -1)
+    # cv2 isn't playing nice with the sample_img files. It keeps trying to open them as color images and if I force it to
+    # treat them as greyscale the intensity values are incorrect. Will use skimage instead.
+    dab_img = skimage.io.imread(str(dab_img_path[0]))
+    sample_img = skimage.io.imread(str(sample_img_path[0]))
     roi = import_roi(str(roi_path[0]))
     roi = roi[roi_path[0].stem]
 
+    # apply the imageJ hand drawn rois here
     # create a mask from the roi drawing
     mask = roi_to_mask(roi)
 
-    # img * mask multiples all the image pixel values by either 0 or 1 to delete areas outside of roi
-    # np.unique counts all the unique values of pixels, in this case cell labels
-    # size calculates the total number of unique cell labels
-    # we subtract 1 because it always detects the background as a unique value, but this isn't a cell... it's background
-    masked_nuc_count = np.unique(nuc_img * mask).size - 1
-    masked_tunel_count = np.unique(stain_img * mask).size - 1
+    dab_img = dab_img * mask
 
-    row_indices = sample_info.loc[sample_info['sample_side'] == sample]
+    # calculate the mean intensity for each cell
+    label_props = skimage.measure.regionprops(dab_img, sample_img)
+    means = np.asarray([d['intensity_mean'] for d in label_props])
+
+    # get all the info into the results table
+    row_indices = sample_info.loc[sample_info['new_filename'] == sample]
     result = []
-    result.append('; '.join([str for str in row_indices.old_filename])) # another 'wonderful' one liner
-    result.append(Path(nuc_img_path[0]).name)
-    result.append(row_indices.iloc[0]['sample'])
+    result.append(row_indices.iloc[0]['new_filename'])
     result.append(row_indices.iloc[0]['treatment'])
+    result.append(row_indices.iloc[0]['time'])
+    result.append(row_indices.iloc[0]['AB'])
+    result.append(row_indices.iloc[0]['new_id'])
     result.append(row_indices.iloc[0]['side'])
-    result.append(row_indices.iloc[0]['section'])
-    result.append(row_indices.iloc[0]['mag'])
-    result.append(masked_nuc_count)
-    result.append(masked_tunel_count)
+
+    # total cells
+    result.append(means.size)
+    # positive cells. sum() only counts True value
+    thresh = row_indices['mean_thresh'][0]
+    result.append( (means < thresh).sum() )
 
     results.append(result)
 
-results_df = pd.DataFrame(results, columns = ['old_filenames', 'new_filename', 'sample', 'treatment', 'side', 'section',  'mag', 'nuclei_count', 'tunel_count'])
+results_df = pd.DataFrame(results, columns = ['new_filename', 'treatment', 'time', 'AB', 'new_id', 'side', 'total_cells', 'DAB_pos'])
 
 ## save
 results_df.to_csv((data_dir / 'results.csv').resolve())
